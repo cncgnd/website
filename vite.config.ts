1e6d0e68bd4197a6ea26a0291719fb213886c109
saksham56 <saksham1387@gmail.com>
2024-04-10 23:42:44 +0530

    config-fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/website",
   plugins: [react()],
   resolve: {
     alias: {

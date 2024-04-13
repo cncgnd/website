812dec023e57bfbb668aa981832f9751945ca09a
saksham56 <saksham1387@gmail.com>
2024-04-13 15:01:03 +0530

    auto-scroll added

---
+++ src/Sections/Events.tsx
@@ -0,0 +1,10 @@
+export const Events = () => {
+  return (
+    <div id="events" className="flex flex-col items-center justify-center bg-black w-screen h-screen">
+      <div>
+        <h2 className="text-white text-3xl font-bold mb-4">Our Events</h2>
+        <div className=" p-4 rounded-lg shadow-lg"></div>
+      </div>
+    </div>
+  );
+};
